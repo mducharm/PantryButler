ebfdc71fc0ce9df1a0461cd7910895a16415a75a
Ryan English <ryeenglish9@gmail.com>
2018-02-18 14:48:22 -0500

    Started Vue for front end

--- PantryButler/app/views/index.py
+++ PantryButler/app/views/index.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def inventory(request):
+    return render(request, 'pb.inv.component.js')
---
+++ PantryButler/public/views/inventory/pb.inv.component.js
@@ -0,0 +1,3 @@
+Vue.component('my-component', {
+  template: '<div>A custom componentygyygygygygygyg!</div>'
+})
