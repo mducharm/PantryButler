5bc8ec31d99401f2de1f93d829dcf79e743689e7
sheveksmath <sheveksmath@gmail.com>
2018-02-18 14:01:57 -0500

    settings.py changes

--- PantryButler/PantryButler/urls.py
+++ PantryButler/PantryButler/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import index
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', index.index, name='index'),
     path('/scripts', index.index, name='index'),
-]
+] 
+
+
